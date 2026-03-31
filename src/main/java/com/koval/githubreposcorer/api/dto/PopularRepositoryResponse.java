90fe8465e742eb26bd8ab1d904e9fc46516506db
Koval <hazardsales@gmail.com>
2026-03-31 23:21:03 +0200

    added comments

--- src/main/java/com/koval/githubreposcorer/api/dto/PopularRepositoryResponse.java
+++ src/main/java/com/koval/githubreposcorer/api/dto/PopularRepositoryResponse.java
@@ -3,13 +3,17 @@ package com.koval.githubreposcorer.api.dto;
 import java.time.Instant;
 
 public record PopularRepositoryResponse(
-        long id,
-        String fullName,
-        String htmlUrl,
-        String language,
-        int stars,
-        int forks,
-        Instant createdAt,
-        Instant pushedAt,
-        double score
+    long id,
+    String fullName,
+    String htmlUrl,
+    String language,
+    int stars,
+    int forks,
+
+    //repo creation time on GitHub
+    Instant createdAt,
+
+    //last time code was pushed to the repo
+    Instant pushedAt,
+    double score
 ) {}
