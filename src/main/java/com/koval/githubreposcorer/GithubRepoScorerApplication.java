1b0e75cf848f3d08afe683e9d6f11eeac6a567f8
Koval <hazardsales@gmail.com>
2026-03-31 19:14:11 +0200

    init commit

---
+++ src/main/java/com/koval/githubreposcorer/GithubRepoScorerApplication.java
@@ -0,0 +1,13 @@
+package com.koval.githubreposcorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepoScorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepoScorerApplication.class, args);
+    }
+
+}
